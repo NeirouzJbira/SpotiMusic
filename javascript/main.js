a1932fe88f305ed99680673f6ed467757faa4264
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-03-25 01:54:22 +0100

    init

---
+++ javascript/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  let $btns = $(".grid-area .button-group button");
+
+  $btns.click(function (e) {
+    $(".grid-area .button-group button").removeClass("active");
+    e.target.classList.add("active");
+
+    let selector = $(e.target).attr("data-filter");
+    $(".grid-area .grid").isotope({
+      filter: selector,
+    });
+
+    return false;
+  });
+});
